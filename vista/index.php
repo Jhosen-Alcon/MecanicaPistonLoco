9c94e355d2015ad221860f820b394731bb6330b4
Jhosen Alcon <jhosen.alcon@gmail.com>
2024-07-09 17:50:06 +0200

    cambio de imagen

--- vista/index.php
+++ vista/index.php
@@ -98,7 +98,7 @@ scratch. This page gets rid of all links and provides the needed markup only.
         <aside class="main-sidebar sidebar-dark-primary elevation-4">
             <!-- Brand Logo -->
             <a href="index.php" class="brand-link">
-                <img src="plantilla/dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
+                <img src="plantilla/dist/img/piston.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
                 <span class="brand-text font-weight-light">MecaniSoft</span>
             </a>
 
